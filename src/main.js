dec4aab70075ae30b2b97a3544abe436b5905f6e
Bilychuk <bilychukkateryna@ukr.net>
2024-03-02 23:39:10 +0200

    fix loadMoreBtn

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ const searchForm = document.querySelector(".form");
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 const loadMoreBtn = document.querySelector(".load-button");
+const imageCard = document.querySelector('.gallery-item');
 const imageLightbox = new SimpleLightbox('.gallery .gallery-link', {
     captionsData: 'alt',
 }); 
@@ -23,13 +24,58 @@ function selectImage(event) {
         return;
     }
 }
+
 searchForm.addEventListener("submit", onSubmit);
+
+loadMoreBtn.addEventListener('click', event => { 
+    loader.classList.remove('is-hidden');
+    current_page++;
+    fetchImages(current_query, 15, current_page)
+        .then(data => {
+            const currentHitsAmount = current_page * 15;
+                if (currentHitsAmount > data.totalHits) {
+                    loadMoreBtn.classList.add('is-hidden');
+                    iziToast.warning({
+                        message: "We're sorry, but you've reached the end of search results.",
+                        messageColor: '#fafafb',
+                        messageSize: '16px',
+                        backgroundColor: '#ef4040',
+                        theme: 'dark',
+                        maxWidth: '432px',
+                    })
+                }
+            const markup = createGalleryMarkup(data.hits);
+            gallery.insertAdjacentHTML("beforeend", markup);
+            loader.classList.add('is-hidden');
+            const rect = imageCard.getBoundingClientRect().height;
+            window.scrollBy({
+                top: rect * 2,
+                behavior: "smooth",
+            });
+        })
+        .catch(error => { 
+            iziToast.error({
+                title: 'Error',
+                titleColor: '#fff',
+                message: 'Error while fetching images from Pixabay!',
+                messageColor: '#fafafb',
+                messageSize: '16px',
+                backgroundColor: '#ef4040',
+                theme: 'dark',
+                iconUrl: errorIcon,
+                maxWidth: '432px',
+            }) 
+        })
+})
 
 function onSubmit(event) {
     event.preventDefault();
     gallery.innerHTML = "";
+    current_page = 1;
+    loader.classList.remove("is-hidden");
     loadMoreBtn.classList.add('is-hidden');
     const query = searchForm.elements.word.value.trim();
+    current_query = query;
 
     if (!query) {
         iziToast.error({
@@ -46,8 +92,6 @@ function onSubmit(event) {
             searchForm.reset()
             return;
     }
-    
-    loader.classList.remove("is-hidden");
 
     fetchImages(query, 15, 1)
         .then(data => {
@@ -68,53 +112,9 @@ function onSubmit(event) {
             }
             const markup = createGalleryMarkup(images);
             gallery.insertAdjacentHTML("beforeend", markup);
-            current_query = query;
-            current_page = 1;
             imageLightbox.refresh();
             loadMoreBtn.classList.remove('is-hidden');
             searchForm.reset();
-            loadMoreBtn.addEventListener('click', event => {
-                event.preventDefault();   
-                loader.classList.remove('is-hidden');
-                current_page++;
-                fetchImages(current_query, 15, current_page)
-                    .then(data => {
-                        const currentHitsAmmount = current_page * 15;
-                         if (currentHitsAmmount > data.totalHits) {
-                loadMoreBtn.classList.add('is-hidden');
-                iziToast.warning({
-                    message: "We're sorry, but you've reached the end of search results.",
-                    messageColor: '#fafafb',
-                    messageSize: '16px',
-                    backgroundColor: '#ef4040',
-                    theme: 'dark',
-                    maxWidth: '432px',
-                })
-            }
-                        const markup = createGalleryMarkup(data.hits);
-                        gallery.insertAdjacentHTML("beforeend", markup);
-                        loader.classList.add('is-hidden');
-                        const imageCard = document.querySelector('.gallery-item');
-                        const rect = imageCard.getBoundingClientRect().height;
-                        window.scrollBy({
-                            top: rect * 2,
-                            behavior: "smooth",
-                        });
-                    })
-                    .catch(error => { 
-                        iziToast.error({
-                            title: 'Error',
-                            titleColor: '#fff',
-                            message: 'Error while fetching images from Pixabay!',
-                            messageColor: '#fafafb',
-                            messageSize: '16px',
-                            backgroundColor: '#ef4040',
-                            theme: 'dark',
-                            iconUrl: errorIcon,
-                            maxWidth: '432px',
-                        }) 
-                    })
-            })
         })
         .catch(error => {
             iziToast.error({
@@ -133,7 +133,3 @@ function onSubmit(event) {
             loader.classList.add("is-hidden");   
         })
 } 
-
- 
-
-
